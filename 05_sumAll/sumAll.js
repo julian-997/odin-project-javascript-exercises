a9331d70c411600b5ffdc96e04c7abb1f030fb61
Julian Ewald <j.ewald97@gmail.com>
2022-09-22 20:06:19 +0200

    Solved exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    
+    if (num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
 
+    if (typeof(num1) !== "number" || typeof(num2) !== "number"){
+        return "ERROR"
+    }
+
+    for (let i = 0; i <= Math.abs(num2 - num1); i++){
+        if (num2 > num1){
+            sum = sum + (num1 + i);
+        }
+        if (num2 < num1){
+            sum = sum + (num2 + i);
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
