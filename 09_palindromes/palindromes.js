97dc4a06576dbc6ad44dfea68dc013e2b64bbaf9
Julian Ewald <j.ewald97@gmail.com>
2022-09-28 23:04:56 +0200

    Solved Exercise 09 - Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+    string = string.replace(/\s+/g,'');
+    string = string.toLowerCase();
+    let reverseString = '';
+    //console.log(string);
+    stringArray = string.split('');
+    for (let i = stringArray.length - 1; i >= 0; i--){
+        reverseString = reverseString + stringArray[i];
+        //console.log(reverseString);
+    }
+    if (string === reverseString) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
+//palindromes('Animal loots foliated detail of stool lamina.');
 // Do not edit below this line
 module.exports = palindromes;
