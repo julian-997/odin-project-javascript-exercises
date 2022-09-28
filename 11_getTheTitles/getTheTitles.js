684e03b22b3cd8a45fc54138448bb62beeb48f2f
Julian Ewald <j.ewald97@gmail.com>
2022-09-28 23:44:55 +0200

    Solved exercise 11 - Get the Titles.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = [];
+    i = 0;
+    for (let book in books){
+        titles.push(books[i].title);
+        i++;
+    }
+    return titles;
 };
 
 // Do not edit below this line
