615c955c17fca586dc8968937a50ea286b2aa0e6
Julian Ewald <j.ewald97@gmail.com>
2022-09-28 23:26:47 +0200

    Solved exercise 10 - Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fibonacciArray = [0, 1];
 
+    if (typeof(num) === "string"){
+        num = parseInt(num);
+    }
+    if (num >= 0){
+        for (let i = 2; i <= num; i++){
+            fibonacciArray[i] = fibonacciArray[i-1] + fibonacciArray[i-2];
+        }
+        return fibonacciArray[num];
+        console.log(fibonacciArray);
+    }
+    else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
